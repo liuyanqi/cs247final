34ad0730ae6cdeee82956bc776b83078e25920fc
Han Sha <13hsha@gmail.com>
2017-10-28 17:29:32 -0400

    added padding

--- extract.py
+++ extract.py
@@ -2,13 +2,15 @@ import numpy as np
 
 f8kraw = open("Flickr8k.token.txt").readlines()
 dic = {}
+maxlen = 0
 
+#better way?
+for line in f8kraw:
+	curlen = len(line.split()) - 1
+	maxlen = curlen if maxlen < curlen else maxlen
 
-# val = f8kraw[0].split()[1:]
-# val.insert(0, 'START')
-# val.append('STOP')
-
-
+# for the longest string
+maxlen = maxlen + 2
 
 for line in f8kraw:
 	key = str(line.split()[0][:-2])
@@ -16,8 +18,10 @@ for line in f8kraw:
 		dic[key] = []
 	val = f8kraw[0].split()[1:]
 	val.insert(0, 'START')
-	val.append('STOP')
+	val.extend(['STOP'] * (maxlen - len(val)))
 	dic[key].append(val)
 
-print dic
+
+
+
 	
