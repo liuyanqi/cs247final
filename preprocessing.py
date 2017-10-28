67bd848bdd85ba823a6d57052bd578dff4f7c3d1
Yanqi Liu <liuyanqi@umich.edu>
2017-10-28 18:26:10 -0400

    update preprocess using pre-trained vgg16

--- preprocessing.py
+++ preprocessing.py
@@ -1,53 +1,62 @@
 import os
-if os.path.exists("/home/yanqi/Desktop/cshw/final_proj/captions_train-val2014.zip"):
-	os.system('wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip')
-	os.system('unzip captions_train-val2014.zip')
-	print "not exist"
 
+import vgg16
+import utils
 
-import json
-from pycocotools.coco import COCO
 import skimage.io as io
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-val = json.load(open('annotations/captions_val2014.json','r'))
-train = json.load(open('annotations/captions_train2014.json','r'))
-
-print(train['images'][1])
-print(train['annotations'][1])
+import tensorflow as tf
+import pickle
 
 
-imgs = train['images']
-annots = train['annotations']
 
-# for efficiency lets group annotations by image
-itoa = {}
-for a in annots:
-    imgid = a['image_id']
-    if not imgid in itoa: itoa[imgid] = []
-    itoa[imgid].append(a)
+train_image_path = "./data/Flicker8k_Dataset/"
+train_image_filename_path = "./data/Flicker8k_text/Flickr_8k.trainImages.txt"
+output_file = "./data/Flicker8k_training_feat.mat"
+train_file_names = []
+target_height = 224
+target_width = 224
+state={"key": []}
+
+with open(train_image_filename_path, 'r') as f:
+	train_file_names = f.read().splitlines()
+
+counter =0
 
 
-out = []
-for i,img in enumerate(imgs):
-    imgid = img['id']
-    
-    # coco specific here, they store train/val images separately
-    loc = 'train2014' if 'train' in img['file_name'] else 'val2014'
-    
-    jimg = {}
-    jimg['file_path'] = os.path.join(loc, img['file_name'])
-    jimg['id'] = imgid
-    
-    sents = []
-    annotsi = itoa[imgid]
-    for a in annotsi:
-        sents.append(a['caption'])
-    jimg['captions'] = sents
-    out.append(jimg)
-    
-json.dump(out, open('coco_raw.json', 'w'))
 
+with tf.device('/cpu:0'):
+	sess = tf.Session()
+	images = tf.placeholder("float", [1, 224, 224, 3])
+	vgg = vgg16.Vgg16("./vgg16.npy")
+	vgg.build(images)
+
+	sess.run(tf.global_variables_initializer())
+
+	for filename in train_file_names:
+		counter+=1
+		print(str(counter) + " / " + str(len(train_file_names)))
+		print(filename)
+		img = Image.open(train_image_path + filename)
+		img = img.resize((224, 224))
+		# img.show()
+		img = np.array(img)
+		img = img.reshape((1, 224, 224, 3))
+
+		
+		feed_dict = {images: img}
+
+		
+		# with tf.name_scope("content_vgg"):
+		# 	vgg.build(images)
+		# # print(vgg.data_dict["conv1_1"])
+
+		feat = sess.run(vgg.relu7, feed_dict=feed_dict)
+		# state[filename] = feat
+
+with open(output_file, 'wb') as f:
+	pickle.dump(state, f)
 
 
